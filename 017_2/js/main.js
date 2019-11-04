eb81dd9df95b4c697bab5cba6ea18d80192a3be8
Sergei <rat87@mail.ru>
2019-11-04 20:20:47 +0300

    017_2 is added

---
+++ 017_2/js/main.js
@@ -0,0 +1,58 @@
+let start = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName("budget-value");
+    daybudgetValue = document.getElementsByClassName("daybudget-value");
+    levelValue = document.getElementsByClassName("level-value");
+    expensesValue = document.getElementsByClassName("expenses-value");
+    optionalexpensesValue = document.getElementsByClassName("optionalexpenses-value");
+    incomeValue = document.getElementsByClassName("income-value");
+    monthsavingsValue = document.getElementsByClassName("monthsavings-value");
+    yearsavingsValue = document.getElementsByClassName("yearsavings-value");
+    yearValue = document.getElementsByClassName("year-value");
+    monthValue = document.getElementsByClassName("month-value");
+    dayValue = document.getElementsByClassName("day-value"),
+    expensesItem = document.getElementsByClassName("expenses-item"),
+    button = document.getElementsByTagName("button");
+    optionalexpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    chooseIncomeLabel = document.querySelector(".choose-income-label"),
+    checksavings = document.querySelector(".checksavings"),
+    chooseSum = document.querySelector(".choose-sum"),
+    choosePercent = document.querySelector(".choose-percent"),
+
+
+console.log(start);
+console.log(budgetValue[0]);
+console.log(daybudgetValue[0]);
+console.log(levelValue[0]);
+console.log(expensesValue[0]);
+console.log(optionalexpensesValue[0]);
+console.log(incomeValue[0]);
+console.log(monthsavingsValue[0]);
+console.log(yearsavingsValue[0]);
+console.log(yearValue[0]);
+console.log(monthValue[0]);
+console.log(dayValue[0]);
+console.log(expensesItem[0]);
+console.log(expensesItem[1]);
+console.log(expensesItem[2]);
+console.log(expensesItem[3]);
+
+let button1 = button[0];
+let button2 = button[1];
+let button3 = button[2];
+
+console.log(button1);
+console.log(button2);
+console.log(button3);
+console.log(optionalexpensesItem[0]);
+console.log(optionalexpensesItem[1]);
+console.log(optionalexpensesItem[2]);
+
+console.log(chooseIncomeLabel);
+console.log(checksavings);
+console.log(chooseSum);
+console.log(choosePercent);
+
+
+
+
+
