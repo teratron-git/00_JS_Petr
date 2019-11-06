cc3280d9e45b374bfaf453e15e066a5ad5a13d35
Sergei <rat87@mail.ru>
2019-11-06 23:26:31 +0300

    Got all variables and remove console log.

--- 017_2/js/main.js
+++ 017_2/js/main.js
@@ -1,58 +1,29 @@
 let start = document.getElementById("start"),
-    budgetValue = document.getElementsByClassName("budget-value");
-    daybudgetValue = document.getElementsByClassName("daybudget-value");
-    levelValue = document.getElementsByClassName("level-value");
-    expensesValue = document.getElementsByClassName("expenses-value");
-    optionalexpensesValue = document.getElementsByClassName("optionalexpenses-value");
-    incomeValue = document.getElementsByClassName("income-value");
-    monthsavingsValue = document.getElementsByClassName("monthsavings-value");
-    yearsavingsValue = document.getElementsByClassName("yearsavings-value");
-    yearValue = document.getElementsByClassName("year-value");
-    monthValue = document.getElementsByClassName("month-value");
+    budgetValue = document.getElementsByClassName("budget-value"),
+    daybudgetValue = document.getElementsByClassName("daybudget-value"),
+    levelValue = document.getElementsByClassName("level-value"),
+    expensesValue = document.getElementsByClassName("expenses-value"),
+    optionalexpensesValue = document.getElementsByClassName("optionalexpenses-value"),
+    incomeValue = document.getElementsByClassName("income-value"),
+    monthsavingsValue = document.getElementsByClassName("monthsavings-value"),
+    yearsavingsValue = document.getElementsByClassName("yearsavings-value"),
+    yearValue = document.getElementsByClassName("year-value"),
+    monthValue = document.getElementsByClassName("month-value"),
     dayValue = document.getElementsByClassName("day-value"),
-    expensesItem = document.getElementsByClassName("expenses-item"),
-    button = document.getElementsByTagName("button");
-    optionalexpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    expensesItem1 = document.getElementsByClassName("expenses-item")[0],
+    expensesItem2 = document.getElementsByClassName("expenses-item")[1],
+    expensesItem3 = document.getElementsByClassName("expenses-item")[2],
+    expensesItem4 = document.getElementsByClassName("expenses-item")[3],
+    button1 = document.getElementsByTagName("button")[0],
+    button2 = document.getElementsByTagName("button")[1],
+    button3 = document.getElementsByTagName("button")[2],
+    optionalexpensesItem1 = document.querySelectorAll(".optionalexpenses-item")[0],
+    optionalexpensesItem2 = document.querySelectorAll(".optionalexpenses-item")[1],
+    optionalexpensesItem3 = document.querySelectorAll(".optionalexpenses-item")[2],
     chooseIncomeLabel = document.querySelector(".choose-income-label"),
     checksavings = document.querySelector(".checksavings"),
     chooseSum = document.querySelector(".choose-sum"),
-    choosePercent = document.querySelector(".choose-percent"),
-
-
-console.log(start);
-console.log(budgetValue[0]);
-console.log(daybudgetValue[0]);
-console.log(levelValue[0]);
-console.log(expensesValue[0]);
-console.log(optionalexpensesValue[0]);
-console.log(incomeValue[0]);
-console.log(monthsavingsValue[0]);
-console.log(yearsavingsValue[0]);
-console.log(yearValue[0]);
-console.log(monthValue[0]);
-console.log(dayValue[0]);
-console.log(expensesItem[0]);
-console.log(expensesItem[1]);
-console.log(expensesItem[2]);
-console.log(expensesItem[3]);
-
-let button1 = button[0];
-let button2 = button[1];
-let button3 = button[2];
-
-console.log(button1);
-console.log(button2);
-console.log(button3);
-console.log(optionalexpensesItem[0]);
-console.log(optionalexpensesItem[1]);
-console.log(optionalexpensesItem[2]);
-
-console.log(chooseIncomeLabel);
-console.log(checksavings);
-console.log(chooseSum);
-console.log(choosePercent);
-
-
+    choosePercent = document.querySelector(".choose-percent");
 
 
 
