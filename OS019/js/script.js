5901133d8efa73d0120a68cab6e0b8ba04f72c41
Sergei <rat87@mail.ru>
2019-11-17 23:28:36 +0300

    Modal JSON request is ready

--- OS019/js/script.js
+++ OS019/js/script.js
@@ -110,7 +110,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
     //Form
 
-    let meggage = {
+    let message = {
         loading: "Загрузка...",
         success: "Спасибо! Скоро мы с вами свяжемся!",
         failure: "Что-то пошло не так..."
@@ -128,10 +128,32 @@ window.addEventListener("DOMContentLoaded", function() {
 
         let request = new XMLHttpRequest();
         request.open("POST", "server.php");
-        request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        // request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        request.setRequestHeader("Content-Type","application/json; charset = UTF-8");
 
         let formData = new FormData(form);
-        request.send(formData);
+
+        let obj ={};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener("readystatechange", function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = ""; 
+        }
     });
 
 });
